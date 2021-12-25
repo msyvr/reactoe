7b98956ae015d4cedbfdcc738274592affe341f3
msyvr <monicaspisar@gmail.com>
2021-12-24 18:35:24 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tictactoe from './components/tictactoe';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <a href = "https://reactjs.org/" target="_blank" ><img src={logo} className="App-logo" alt="logo" /></a>
+        <a href = "https://github.com/msyvr/reactoe" target = "_blank">play tic tac toe</a>
+      </div>
+      <div className="App-body">
+        <Tictactoe />
+      </div>
     </div>
   );
 }
